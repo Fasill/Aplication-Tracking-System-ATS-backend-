9cdf754541afd5dca788cc3da60a97568cb7b007
fasil <fasilhawultie19@gmail.com>
2023-12-05 15:02:38 +0300

    some features deleted to debug

--- models/User.js
+++ models/User.js
@@ -1,13 +1,11 @@
-import { getStorage, ref, uploadBytesResumable, getDownloadURL, deleteObject } from 'firebase/storage';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { initializeApp } from 'firebase/app';
-import { giveCurrentDateTime } from '../utils/DateAndTime.js'
+import { giveCurrentDateTime } from '../utils/DateAndTime.js';
 
-import admin from "firebase-admin"
-
+import admin from "firebase-admin";
 import serviceAccount from "../credentials/serviceAccountKey.json"  assert { type: "json" };
-// import * as serviceAccountKey from './serviceAccountKey.json';
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyBcuAGCyGIzzP0Fv39ezoFZx1d-Vk3SHts",
   authDomain: "seventh-vigil-390113.firebaseapp.com",
   projectId: "seventh-vigil-390113",
@@ -15,28 +13,26 @@ export const firebaseConfig = {
   messagingSenderId: "93104458846",
   appId: "1:93104458846:web:8d1f80d813f017ed9bb8cf",
   measurementId: "G-RYFZMQHPBE",
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 };
+
+// Initialize admin
 admin.initializeApp(firebaseConfig);
 
-initializeApp(firebaseConfig)
+// Initialize the regular Firebase app for storage
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
 const db = admin.firestore();
-
-
-
 
 export const otpRef  = db.collection("otp");
 export const Users = db.collection('User');
 export const Candidates = db.collection('Candidates');
-
 export const Companies = db.collection('Companies');
 export const Jobs = db.collection('Jobs');
 export const Comments = db.collection('Comments');
 
-
-
-const storage = getStorage();
-
+// Add the uploadresume function
 export const uploadresume = async (req) => {
   try {
     const dateTime = giveCurrentDateTime();
@@ -48,18 +44,6 @@ export const uploadresume = async (req) => {
     const downloadURL = await getDownloadURL(snapshot.ref);
 
     return downloadURL;
-  } catch (e) {
-    console.error(e);
-    throw new Error('Internal server error');
-  }
-};
-
-export const deleteResume = async (resumeUrl) => {
-  try {
-    const storageRef = ref(storage, resumeUrl);
-
-    // Delete the resume from Firestore Storage
-    await deleteObject(storageRef);
   } catch (e) {
     console.error(e);
     throw new Error('Internal server error');
