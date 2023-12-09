5f37806700d25ba50a8a35ae25bbde6c9d994824
fasil <fasilhawultie19@gmail.com>
2023-12-09 16:33:15 +0300

    debug: debug wehn importing function

--- controllers/clientControllers/candidatetoken.js
+++ controllers/clientControllers/candidatetoken.js
@@ -1,9 +1,8 @@
 import { generateTokenForClient } from "../../utils/tokenGenerator.js";
-import { getEmailFromToken } from "../../utils/decodeTokenAndGetId";
+import { getEmailFromToken } from "../../utils/decodeTokenAndGetId.js";
 
 export const candidatetoken = (req,res)=>{
     const {email} = req.body;
-    
    const token =  generateTokenForClient(email)
     res.send({"email":getEmailFromToken(token),"token":token})
 }
